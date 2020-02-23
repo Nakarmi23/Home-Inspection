bfdb6d96f8d31167b54b98da941495c5732a046d
Ankit Nakarmi <ankit.nakarmi23@gmail.com>
2020-02-23 22:06:08 +0545

    created app theme

---
+++ lib/theme/theme.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+ThemeData appData() {
+  return ThemeData(
+    primaryColor: Color(0xff008e4b),
+    primaryColorLight: Color(0xff4dbf78),
+    primaryColorDark: Color(0xff005f21),
+    primarySwatch: Colors.green,
+    accentColor: Color(0xff2e3192),
+    backgroundColor: Colors.grey.shade50,
+  );
+}
