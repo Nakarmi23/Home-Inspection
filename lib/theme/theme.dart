9c849d1758268a8d5e6bc2d1bfcf9231b0cef65a
Ankit Nakarmi <ankit.nakarmi23@gmail.com>
2020-03-10 22:41:37 +0545

    changed app theme

--- lib/theme/theme.dart
+++ lib/theme/theme.dart
@@ -2,11 +2,11 @@ import 'package:flutter/material.dart';
 
 ThemeData appData() {
   return ThemeData(
-    primaryColor: Color(0xff008e4b),
-    primaryColorLight: Color(0xff4dbf78),
-    primaryColorDark: Color(0xff005f21),
-    primarySwatch: Colors.green,
-    accentColor: Color(0xff2e3192),
+    primaryColor: Color(0xff2e3192),
+    primaryColorLight: Color(0xff635bc3),
+    primaryColorDark: Color(0xff000c63),
+    primarySwatch: Colors.blue,
+    accentColor: Color(0xff008e4b),
     backgroundColor: Colors.grey.shade50,
   );
 }
