2ac09d6b61ce0e5f9f54611278a8aa85538a5b3e
Ankit Nakarmi <ankit.nakarmi23@gmail.com>
2020-03-10 21:35:54 +0545

    removed contact from client

--- lib/models/IClient.dart
+++ lib/models/IClient.dart
@@ -4,25 +4,21 @@ class IClient {
   int id;
   String name;
   String address;
-  String contact;
 
   IClient({
     this.id,
     this.name,
     this.address,
-    this.contact,
   });
 
   IClient.fromJSON(Map<String, dynamic> json) {
     id = json['id'];
     name = json['name'];
     address = json['address'];
-    contact = json['contact'];
   }
   Map<String, dynamic> toJson() => {
         'id': id,
         'name': name,
         'address': address,
-        'contact': contact,
       };
 }
