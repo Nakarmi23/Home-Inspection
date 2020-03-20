ff85642366049687f429f8a212b083da88ffdc9c
Ankit Nakarmi <ankit.nakarmi23@gmail.com>
2020-03-20 22:57:04 +0545

    implemented functions for Client SQLIET

--- lib/services/sqflite_client_service.dart
+++ lib/services/sqflite_client_service.dart
@@ -1,27 +1,27 @@
 import 'package:house_review/models/IClient.dart';
 import 'package:house_review/services/sqflite_base_service.dart';
+import 'package:sqflite/sqflite.dart';
 
 class SqliteClientService extends SqliteBaseService<IClient> {
   @override
-  // TODO: implement tableName
   String get tableName => 'client';
 
   @override
-  Future<int> count() {
-    // TODO: implement count
-    return null;
+  Future<int> count() async {
+    final List<Map<String, dynamic>> dataRow =
+        await (await db).query(tableName, columns: ['count(1)']);
+    return Sqflite.firstIntValue(dataRow);
   }
 
   @override
-  Future<void> delete(int id) {
-    // TODO: implement delete
-    return null;
+  Future<void> delete(int id) async {
+    return (await db).delete(tableName, where: 'id = ?', whereArgs: [id]);
   }
 
   @override
-  Future<void> insert(data) {
-    // TODO: implement insert
-    return null;
+  Future<void> insert(IClient data) async {
+    return (await db).insert(tableName, data.toJson(),
+        conflictAlgorithm: ConflictAlgorithm.replace);
   }
 
   @override
@@ -34,8 +34,9 @@ class SqliteClientService extends SqliteBaseService<IClient> {
   }
 
   @override
-  Future<void> update() {
-    // TODO: implement update
-    return null;
+  Future<void> update({IClient data}) async {
+    Map<String, dynamic> jsonData = data.toJson();
+    return (await db)
+        .update(tableName, jsonData, where: "id = ?", whereArgs: [data.id]);
   }
 }
