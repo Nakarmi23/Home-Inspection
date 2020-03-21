49223b1be786068c4e15bf574c95f2947b25b375
Ankit Nakarmi <ankit.nakarmi23@gmail.com>
2020-03-21 14:36:39 +0545

    instered default data in structural system if data doesnot exists

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:house_review/models/IStructuralSystem.dart';
+import 'package:house_review/services/sqflite_structural_system_service.dart';
 
 class SplashScreen extends StatefulWidget {
   SplashScreen({Key key}) : super(key: key);
@@ -13,6 +15,16 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
+    SqliteStructuralSysService().count().then((count) => {
+          if (count == 0)
+            {
+              ['RCC Framed Structure', 'Masonary Structure', 'Steel Structure']
+                  .forEach((item) => {
+                        SqliteStructuralSysService()
+                            .insert(IStructuralSystem(systemName: item))
+                      })
+            }
+        });
     Timer(
       Duration(seconds: 2),
       () => Navigator.of(context).pushReplacementNamed('/inspectionForm'),
