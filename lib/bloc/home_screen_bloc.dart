f44282d5d7b23fe29b5cbc43198e2219b1ad8266
Ankit Nakarmi <ankit.nakarmi23@gmail.com>
2020-06-13 18:32:25 +0545

    created getdata method with pagenation

--- lib/bloc/home_screen_bloc.dart
+++ lib/bloc/home_screen_bloc.dart
@@ -1,1 +1,30 @@
-class HomeScreenBloc {}
+import 'dart:async';
+
+import 'package:house_review/models/IClient.dart';
+import 'package:house_review/resources/root_repository.dart';
+
+class HomeScreenBloc {
+  final RootRepository _repository = RootRepository();
+  int _clientCount = 0;
+  int _currentCount = 0;
+  StreamController<List<IClient>> _client = StreamController<List<IClient>>();
+
+  Stream<List<IClient>> get client => _client.stream;
+
+  Future getClientCount() {
+    return _repository.clientService
+        .count()
+        .then((value) => {_clientCount = value})
+        .catchError((err) => throw err);
+  }
+
+  void getClientData() {
+    if (_currentCount < _clientCount)
+      _repository.clientService
+          .select(limit: 20, offset: _currentCount, orderBy: 'id desc')
+          .then((value) {
+        _currentCount = value.length;
+        _client.sink.add(value);
+      }).catchError((err) => throw err);
+  }
+}
