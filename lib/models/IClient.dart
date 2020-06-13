c27335cf83628e38fa32bd6197ef439546e96d83
Ankit Nakarmi <ankit.nakarmi23@gmail.com>
2020-06-13 15:33:04 +0545

    add new attribute 'DateOfInspection'

--- lib/models/IClient.dart
+++ lib/models/IClient.dart
@@ -4,18 +4,16 @@ class IClient extends IBaseModel<IClient> {
   int id;
   String name;
   String address;
+  DateTime dateOfInspection;
 
-  IClient({
-    this.id,
-    this.name,
-    this.address,
-  });
+  IClient({this.id, this.name, this.address, this.dateOfInspection});
 
   @override
   Map<String, dynamic> toJson() => {
         'id': id,
         'name': name,
         'address': address,
+        'date_of_inspection': dateOfInspection.toIso8601String()
       };
 
   @override
@@ -23,6 +21,7 @@ class IClient extends IBaseModel<IClient> {
     id = json['id'];
     name = json['name'];
     address = json['address'];
+    dateOfInspection = DateTime.parse(json['date_of_inspection']);
     return this;
   }
 }
