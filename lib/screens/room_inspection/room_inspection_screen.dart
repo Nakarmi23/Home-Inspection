1b46075c3a963db0c6489bead17ee0e1cb665abe
Ankit Nakarmi <ankit.nakarmi23@gmail.com>
2020-06-20 11:52:54 +0545

    created room inspection form

---
+++ lib/screens/room_inspection/room_inspection_screen.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/material.dart';
+
+class RoomInspectionScreen extends StatefulWidget {
+  final String title;
+  RoomInspectionScreen({Key key, this.title}) : super(key: key);
+
+  @override
+  _RoomInspectionScreenState createState() => _RoomInspectionScreenState();
+}
+
+class _RoomInspectionScreenState extends State<RoomInspectionScreen> {
+  @override
+  Widget build(BuildContext context) {
+    var appBar = SliverAppBar(
+      expandedHeight: 300.0,
+      title: Text(
+        ModalRoute.of(context).settings.arguments,
+      ),
+      centerTitle: true,
+      floating: true,
+      pinned: true,
+      bottom: PreferredSize(
+        child: Container(
+          color: Theme.of(context).primaryColor,
+          child: Stack(
+            overflow: Overflow.visible,
+            children: <Widget>[
+              TabBar(
+                isScrollable: true,
+                tabs: [
+                  Tab(
+                    text: 'Structural Inspection',
+                  ),
+                  Tab(
+                    text: 'Water Quality',
+                  ),
+                  Tab(
+                    text: 'Luxmeter Reading',
+                  ),
+                  Tab(
+                    text: 'Seepage Analysis',
+                  ),
+                  Tab(
+                    text: 'Visual Inspection',
+                  ),
+                  Tab(
+                    text: 'Minor Checks',
+                  ),
+                  Tab(
+                    text: 'Kitchen Inspection',
+                  ),
+                  Tab(
+                    text: 'Toilet Inspection',
+                  ),
+                  Tab(
+                    text: 'Staircase Inspection',
+                  ),
+                ],
+              ),
+              Positioned(
+                bottom: -30,
+                right: 16.0,
+                child: SizedBox.fromSize(
+                  size: Size(60.0, 60.0),
+                  child: FloatingActionButton(
+                    child: Icon(Icons.camera_enhance),
+                    onPressed: () {},
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        preferredSize: Size.fromHeight(kToolbarHeight),
+      ),
+      flexibleSpace: FlexibleSpaceBar(
+        background: Image.network(
+          'https://images.unsplash.com/photo-1565538810643-b5bdb714032a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+    return Scaffold(
+      body: DefaultTabController(
+        length: 9,
+        child: CustomScrollView(
+          slivers: <Widget>[
+            appBar,
+            SliverFillRemaining(
+              child: TabBarView(
+                children: <Widget>[
+                  Text('1'),
+                  Text('2'),
+                  Text('3'),
+                  Text('4'),
+                  Text('5'),
+                  Text('6'),
+                  Text('7'),
+                  Text('8'),
+                  Text('9'),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
