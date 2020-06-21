69f9259d043f9f3f18c91457a7a4ec885027e8ab
Ankit Nakarmi <ankit.nakarmi23@gmail.com>
2020-06-21 11:46:57 +0545

    completed water quality form

--- lib/screens/room_inspection/forms/water_quality_form.dart
+++ lib/screens/room_inspection/forms/water_quality_form.dart
@@ -5,9 +5,88 @@ class WaterQualityForm extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text('Water Quality Form'),
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(top: 16.0, left: 16.0, right: 16.0),
+            child: HeadingText('Sample 1'),
+          ),
+          AppInputTextField(
+            labelText: 'Sample Source',
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: 16.0, left: 16.0, right: 16.0),
+            child: SubHeadingText('Sample 1 - Upload Photo'),
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: 16.0, left: 16.0, right: 16.0),
+            child: SizedBox(
+              height: 80,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: 1,
+                itemBuilder: (context, index) {
+                  return SizedBox(
+                    height: 80,
+                    width: 80,
+                    child: Material(
+                      borderRadius: BorderRadius.circular(7.0),
+                      color: Colors.grey.shade300,
+                      child: InkWell(
+                        borderRadius: BorderRadius.circular(7.0),
+                        onTap: () {},
+                        child: Center(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Icon(
+                                Icons.add,
+                                color: Colors.grey.shade600,
+                              ),
+                              Text(
+                                'Add Photo',
+                                style: TextStyle(color: Colors.grey.shade600),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ),
+          AppInputTextField(
+            labelText: ' Color',
+          ),
+          AppInputTextField(
+            labelText: ' Taste',
+          ),
+          AppInputTextField(
+            labelText: 'Odour',
+          ),
+          AppInputTextField(
+            labelText: ' PH Value',
+          ),
+          AppInputTextField(
+            labelText: ' Turbuduty',
+          ),
+          AppInputTextField(
+            labelText: ' EC Meter Reading',
+          ),
+          AppInputTextField(
+            labelText: ' Temperature',
+          ),
+          AppInputTextField(
+            labelText: ' TDS Meter Reading',
+          ),
+          AppInputTextField(
+            labelText: ' Chlorine Meter Reading',
+          ),
+        ],
       ),
     );
   }
