8b463449bc46233e93c3fe71f7e8efe82983d122
Ankit Nakarmi <ankit.nakarmi23@gmail.com>
2020-07-11 10:20:35 +0545

    added padding after keyboard appears

--- lib/screens/room_inspection/forms/structural_inspection_form.dart
+++ lib/screens/room_inspection/forms/structural_inspection_form.dart
@@ -2,7 +2,6 @@ part of '../room_inspection_screen.dart';
 
 class StructuralInspectionForm extends StatelessWidget {
   const StructuralInspectionForm({Key key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
--- lib/screens/room_inspection/room_inspection_screen.dart
+++ lib/screens/room_inspection/room_inspection_screen.dart
@@ -90,23 +90,28 @@ class _RoomInspectionScreenState extends State<RoomInspectionScreen> {
       ),
     );
     return Scaffold(
+      resizeToAvoidBottomPadding: false,
       body: DefaultTabController(
         length: 8,
         child: NestedScrollView(
           headerSliverBuilder: (context, innerBoxIsScrolled) {
             return [appBar];
           },
-          body: TabBarView(
-            children: <Widget>[
-              StructuralInspectionForm(),
-              WaterQualityForm(),
-              LuxmeterReadingForm(),
-              SeepageAnalysisFrom(),
-              MinorChecksForm(),
-              KitchenInspectionForm(),
-              ToiletInspectionForm(),
-              StaircaseInspectionForm(),
-            ],
+          body: Padding(
+            padding: EdgeInsets.only(
+                bottom: MediaQuery.of(context).viewInsets.bottom),
+            child: TabBarView(
+              children: <Widget>[
+                StructuralInspectionForm(),
+                WaterQualityForm(),
+                LuxmeterReadingForm(),
+                SeepageAnalysisFrom(),
+                MinorChecksForm(),
+                KitchenInspectionForm(),
+                ToiletInspectionForm(),
+                StaircaseInspectionForm(),
+              ],
+            ),
           ),
         ),
       ),
