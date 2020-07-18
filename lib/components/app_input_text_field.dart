277857342d9084db89f5ccfa15fe3f01841bdc43
Ankit Nakarmi <ankit.nakarmi23@gmail.com>
2020-07-18 09:15:29 +0545

    refactored code and added new paramas

--- lib/components/app_input_text_field.dart
+++ lib/components/app_input_text_field.dart
@@ -6,13 +6,19 @@ class AppInputTextField extends StatelessWidget {
     String labelText,
     bool enabled,
     TextInputType keyboardType,
+    Function(String) onChange,
+    Function(String) onFieldSubmitted,
   })  : _labelText = labelText,
         _enabled = enabled == null ? true : enabled,
         _keyboardType = keyboardType,
+        _onChanged = onChange,
+        _onFieldSubmitted = onFieldSubmitted,
         super(key: key);
   final String _labelText;
   final bool _enabled;
   final TextInputType _keyboardType;
+  final Function(String) _onChanged;
+  final Function(String) _onFieldSubmitted;
   @override
   Widget build(BuildContext context) {
     OutlineInputBorder outlineInputBorder = OutlineInputBorder(
@@ -25,22 +31,17 @@ class AppInputTextField extends StatelessWidget {
 
     return Padding(
       padding: EdgeInsets.symmetric(horizontal: 16.0).copyWith(top: 16.0),
-      child: Column(
-        children: <Widget>[
-          // SizedBox.fromSize(
-          //   size: Size.fromHeight(16),
-          // ),
-          TextFormField(
-            key: key,
-            keyboardType: _keyboardType,
-            enabled: _enabled,
-            decoration: InputDecoration(
-              border: outlineInputBorder,
-              enabledBorder: outlineInputBorder,
-              labelText: _labelText,
-            ),
-          )
-        ],
+      child: TextFormField(
+        key: key,
+        keyboardType: _keyboardType,
+        enabled: _enabled,
+        onChanged: _onChanged,
+        onFieldSubmitted: _onFieldSubmitted,
+        decoration: InputDecoration(
+          border: outlineInputBorder,
+          enabledBorder: outlineInputBorder,
+          labelText: _labelText,
+        ),
       ),
     );
   }
