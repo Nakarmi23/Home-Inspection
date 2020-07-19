8d307c95f086257213cc8f254eb3834f3560394e
Ankit Nakarmi <ankit.nakarmi23@gmail.com>
2020-07-19 21:10:20 +0545

    refactored code and addded tab view

--- lib/screens/room_inspection/forms/structural_inspection_form.dart
+++ lib/screens/room_inspection/forms/structural_inspection_form.dart
@@ -4,9 +4,46 @@ class StructuralInspectionForm extends StatelessWidget {
   const StructuralInspectionForm({Key key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
+    return DefaultTabController(
+      length: 2,
       child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Container(
+            child: TabBar(
+              onTap: (value) {},
+              tabs: [
+                Tab(
+                  text: 'Visual Inspection Sheet',
+                ),
+                Tab(text: 'Non Destructive Test'),
+              ],
+            ),
+            color: Theme.of(context).primaryColor,
+          ),
+          Expanded(
+            child: TabBarView(
+              physics: NeverScrollableScrollPhysics(),
+              children: [
+                VisualInspectionSheetView(),
+                NonDestructiveTestView(),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class VisualInspectionSheetView extends StatelessWidget {
+  const VisualInspectionSheetView({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: ListView(
+        padding:
+            EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
         children: <Widget>[
           Padding(
             padding: EdgeInsets.only(top: 16.0, left: 16.0, right: 16.0),
@@ -323,110 +360,124 @@ class StructuralInspectionForm extends StatelessWidget {
             ),
             onTap: () {},
           ),
-          Padding(
-            padding: EdgeInsets.only(top: 16.0, left: 16.0, right: 16.0),
-            child: HeadingText('Non Destructive Test'),
+        ],
+      ),
+    );
+  }
+}
+
+class NonDestructiveTestView extends StatelessWidget {
+  const NonDestructiveTestView({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      padding:
+          EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
+      children: <Widget>[
+        Padding(
+          padding: EdgeInsets.only(top: 16.0, left: 16.0, right: 16.0),
+          child: HeadingText('Non Destructive Test'),
+        ),
+        Padding(
+          padding: EdgeInsets.only(top: 16.0, left: 16.0, right: 16.0),
+          child: SubHeadingText(
+            'Non Destructive Test - Photo using Schmidt Hammer',
           ),
-          Padding(
-            padding: EdgeInsets.only(top: 16.0, left: 16.0, right: 16.0),
-            child: SubHeadingText(
-              'Non Destructive Test - Photo using Schmidt Hammer',
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.only(top: 16.0, left: 16.0, right: 16.0),
-            child: SizedBox(
-              height: 80,
-              child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                itemCount: 1,
-                itemBuilder: (context, index) {
-                  return SizedBox(
-                    height: 80,
-                    width: 80,
-                    child: Material(
+        ),
+        Padding(
+          padding: EdgeInsets.only(top: 16.0, left: 16.0, right: 16.0),
+          child: SizedBox(
+            height: 80,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: 1,
+              itemBuilder: (context, index) {
+                return SizedBox(
+                  height: 80,
+                  width: 80,
+                  child: Material(
+                    borderRadius: BorderRadius.circular(7.0),
+                    color: Colors.grey.shade300,
+                    child: InkWell(
                       borderRadius: BorderRadius.circular(7.0),
-                      color: Colors.grey.shade300,
-                      child: InkWell(
-                        borderRadius: BorderRadius.circular(7.0),
-                        onTap: () {},
-                        child: Center(
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: <Widget>[
-                              Icon(
-                                Icons.add,
-                                color: Colors.grey.shade600,
-                              ),
-                              Text(
-                                'Add Photo',
-                                style: TextStyle(color: Colors.grey.shade600),
-                              ),
-                            ],
-                          ),
+                      onTap: () {},
+                      child: Center(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Icon(
+                              Icons.add,
+                              color: Colors.grey.shade600,
+                            ),
+                            Text(
+                              'Add Photo',
+                              style: TextStyle(color: Colors.grey.shade600),
+                            ),
+                          ],
                         ),
                       ),
                     ),
-                  );
-                },
+                  ),
+                );
+              },
+            ),
+          ),
+        ),
+        AppInputTextField(
+          labelText: ' Element of Structure',
+        ),
+        AppInputTextField(
+          labelText: ' Concrete Grade',
+        ),
+        AppInputTextField(
+          labelText: ' Direction of Impact',
+        ),
+        AppInputTextField(
+          labelText: ' Location',
+        ),
+        Padding(
+          padding: EdgeInsets.only(top: 16.0, left: 16.0, right: 16.0),
+          child: SubHeadingText(
+            'Non Destructive Test - Reading',
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.only(top: 16.0, left: 16.0, right: 16.0),
+          child: DataTable(
+            columns: [
+              DataColumn(label: Text('S.N.')),
+              DataColumn(label: Text('Readings')),
+            ],
+            rows: [
+              DataRow(
+                cells: [
+                  DataCell(
+                    Text('1'),
+                    placeholder: true,
+                  ),
+                  DataCell(
+                    Text('Enter Readings Here'),
+                    placeholder: true,
+                    showEditIcon: true,
+                  )
+                ],
               ),
-            ),
+              DataRow(
+                cells: [
+                  DataCell(
+                    Text('Average (1 to 10)'),
+                  ),
+                  DataCell(
+                    Text('Auto Generated'),
+                    onTap: null,
+                  )
+                ],
+              ),
+            ],
           ),
-          AppInputTextField(
-            labelText: ' Element of Structure',
-          ),
-          AppInputTextField(
-            labelText: ' Concrete Grade',
-          ),
-          AppInputTextField(
-            labelText: ' Direction of Impact',
-          ),
-          AppInputTextField(
-            labelText: ' Location',
-          ),
-          Padding(
-            padding: EdgeInsets.only(top: 16.0, left: 16.0, right: 16.0),
-            child: SubHeadingText(
-              'Non Destructive Test - Reading',
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.only(top: 16.0, left: 16.0, right: 16.0),
-            child: DataTable(
-              columns: [
-                DataColumn(label: Text('S.N.')),
-                DataColumn(label: Text('Readings')),
-              ],
-              rows: [
-                DataRow(
-                  cells: [
-                    DataCell(
-                      Text('1'),
-                      placeholder: true,
-                    ),
-                    DataCell(
-                      Text('Enter Readings Here'),
-                      placeholder: true,
-                      showEditIcon: true,
-                    )
-                  ],
-                ),
-                DataRow(
-                  cells: [
-                    DataCell(
-                      Text('Average (1 to 10)'),
-                    ),
-                    DataCell(
-                      Text('Auto Generated'),
-                      onTap: null,
-                    )
-                  ],
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
