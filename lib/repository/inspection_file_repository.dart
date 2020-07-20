3d871677d888ac3ded11f69c4f21d278b14d0b4e
Ankit Nakarmi <ankit.nakarmi23@gmail.com>
2020-07-20 21:08:18 +0545

    created inspection file repo to read ad write inspection data

---
+++ lib/repository/inspection_file_repository.dart
@@ -0,0 +1,45 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:house_review/models/inspection_data.dart';
+import 'package:path_provider/path_provider.dart';
+
+class InspectionFileRepository {
+  Future<String> get _localPath async {
+    final directory = await getApplicationDocumentsDirectory();
+
+    return directory.path;
+  }
+
+  Future<File> _localFile(String filename) async {
+    final path = await _localPath;
+    return File('$path/$filename');
+  }
+
+  Future<File> writeFile(InspectionData inspectionData) async {
+    final file = await _localFile(
+        '${inspectionData.name}${DateTime.now().toIso8601String()}');
+
+    // Write the file.
+    return file.writeAsBytes(
+      utf8.encode(
+        json.encode(
+          inspectionData.toJson(),
+        ),
+      ),
+    );
+  }
+
+  Future<InspectionData> readFile(String filename) async {
+    try {
+      final file = await _localFile(filename);
+
+      // Read the file.
+      List<int> byteContent = await file.readAsBytes();
+
+      return InspectionData.fromJson(json.decode(utf8.decode(byteContent)));
+    } catch (err) {
+      throw err;
+    }
+  }
+}
