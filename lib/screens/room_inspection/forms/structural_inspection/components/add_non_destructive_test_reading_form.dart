26c03cb8d61bc04ff947cb48baa3c3227b190723
Ankit Nakarmi <ankit.nakarmi23@gmail.com>
2020-07-28 13:41:11 +0545

    created add non destructive test reading form

---
+++ lib/screens/room_inspection/forms/structural_inspection/components/add_non_destructive_test_reading_form.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:house_review/components/app_input_text_field.dart';
+import 'package:house_review/models/structural_inspection/structural_inspection.dart';
+
+class AddNonDestructiveTestReadingForm extends StatelessWidget {
+  const AddNonDestructiveTestReadingForm(
+      {Key key, @required this.onFormSave, this.toEditValue})
+      : assert(onFormSave != null),
+        super(key: key);
+  final ValueChanged<String> onFormSave;
+  final String toEditValue;
+  @override
+  Widget build(BuildContext context) {
+    GlobalKey<FormState> formKey = GlobalKey();
+    String reading = toEditValue;
+    return Form(
+      key: formKey,
+      autovalidate: true,
+      child: Column(
+        children: <Widget>[
+          AppInputTextField(
+            labelText: 'Reading',
+            initialValue: toEditValue,
+            validator: (value) {
+              switch (value) {
+                case '':
+                  return 'Reading cannot be empty';
+                default:
+              }
+            },
+            onSaved: (newValue) {
+              reading = newValue;
+            },
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0)
+                .copyWith(top: 16.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                RaisedButton(
+                  color: Theme.of(context).primaryColor,
+                  textColor: Colors.white,
+                  onPressed: () {
+                    if (formKey.currentState.validate()) {
+                      formKey.currentState.save();
+                      onFormSave(reading);
+                    }
+                  },
+                  child: Text(toEditValue == null ? 'Add' : 'Edit'),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
