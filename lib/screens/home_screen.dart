2a198669affee44b3cc7568a06973dce85587984
Ankit Nakarmi <ankit.nakarmi23@gmail.com>
2020-08-02 17:50:02 +0545

    made the data deletable

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_cubit/flutter_cubit.dart';
+import 'package:house_review/components/delete_item_dialog.dart';
+import 'package:house_review/components/dismissible_background.dart';
 import 'package:house_review/cubit/home_inspection_cubit/home_inspection_cubit.dart';
 import 'package:house_review/cubit/inspection_file_info_cubit/inspection_file_info_cubit.dart';
 import 'package:house_review/models/inspection_data.dart';
-import 'package:house_review/models/inspection_file_info.dart';
 
 class HomeScreen extends StatefulWidget {
   HomeScreen({Key key}) : super(key: key);
@@ -42,6 +43,17 @@ class _HomeScreenState extends State<HomeScreen> {
     return CubitBuilder<InspectionFileInfoCubit, InspectionFileInfoState>(
       builder: (context, state) {
         return Scaffold(
+          appBar: AppBar(
+            title: Text('House Inspection'),
+            centerTitle: true,
+            actions: <Widget>[
+              IconButton(
+                icon: Icon(Icons.search),
+                tooltip: 'Search',
+                onPressed: () {},
+              )
+            ],
+          ),
           floatingActionButton: FloatingActionButton(
             onPressed: () async {
               shouldListen = false;
@@ -51,78 +63,85 @@ class _HomeScreenState extends State<HomeScreen> {
             child: Icon(Icons.add),
             tooltip: 'Create New Inspection',
           ),
-          body: CustomScrollView(
-            slivers: <Widget>[
-              SliverAppBar(
-                title: Text('House Inspection'),
-                centerTitle: true,
-                actions: <Widget>[
-                  IconButton(
-                    icon: Icon(Icons.search),
-                    tooltip: 'Search',
-                    onPressed: () {},
-                  )
-                ],
-                floating: true,
-                pinned: false,
-                snap: false,
-              ),
-              CubitListener<HomeInspectionCubit, HomeInspectionState>(
-                listener: (context, state) async {
-                  if (state is HomeInspectionSuccess) {
-                    shouldListen = false;
-                    await Navigator.of(context)
-                        .pushNamed('/inspectionForm', arguments: true);
-                    shouldListen = true;
-                  }
-                },
-                listenWhen: (previous, current) {
-                  return shouldListen;
-                },
-                child: CubitBuilder<InspectionFileInfoCubit,
-                    InspectionFileInfoState>(
-                  builder: (context, state) {
-                    if (state is InspectionFileInfoSuccess) {
-                      return SliverList(
-                        delegate: SliverChildBuilderDelegate(
-                          (context, index) {
-                            if (index.isOdd) {
-                              return Divider(
-                                height: 0,
-                                indent: (32.0 * 2),
-                              );
-                            }
-                            return ListTile(
-                              onTap: () async {
-                                context.cubit<HomeInspectionCubit>().readData(
-                                    state.inspectionFileInfos[index ~/ 2]
-                                        .fileName);
-                              },
-                              leading: Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: <Widget>[
-                                  Text(
-                                    (index ~/ 2).toString(),
-                                    style:
-                                        Theme.of(context).textTheme.headline4,
-                                  ),
-                                ],
-                              ),
-                              title: Text(
-                                  state.inspectionFileInfos[index ~/ 2].name),
-                              subtitle: Text(state
-                                  .inspectionFileInfos[index ~/ 2].address),
-                            );
+          body: CubitListener<HomeInspectionCubit, HomeInspectionState>(
+            listener: (context, state) async {
+              if (state is HomeInspectionSuccess) {
+                shouldListen = false;
+                await Navigator.of(context)
+                    .pushNamed('/inspectionForm', arguments: true);
+                shouldListen = true;
+              }
+            },
+            listenWhen: (previous, current) {
+              return shouldListen;
+            },
+            child: Builder(
+              builder: (context) {
+                if (state is InspectionFileInfoInProcess) {
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+                if (state is InspectionFileInfoSuccess) {
+                  return ListView.separated(
+                    itemBuilder: (context, index) {
+                      return Dismissible(
+                        direction: DismissDirection.endToStart,
+                        background: DismissibleBackground(
+                          color: Theme.of(context).errorColor,
+                          forDirection: DismissDirection.endToStart,
+                          icon: Icon(
+                            Icons.delete,
+                            color: Colors.white,
+                          ),
+                          child: Text(
+                            'Delete',
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                        key: UniqueKey(),
+                        confirmDismiss: (direction) async {
+                          bool isConfirm = await deleteItemAlertModel(context);
+                          return isConfirm;
+                        },
+                        onDismissed: (direction) {
+                          context
+                              .cubit<InspectionFileInfoCubit>()
+                              .deleteData(state.inspectionFileInfos[index]);
+                        },
+                        child: ListTile(
+                          onTap: () async {
+                            context.cubit<HomeInspectionCubit>().readData(
+                                state.inspectionFileInfos[index].fileName);
                           },
-                          childCount:
-                              (state.inspectionFileInfos.length * 2) - 1,
+                          leading: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Text(
+                                (index).toString(),
+                                style: Theme.of(context).textTheme.headline4,
+                              ),
+                            ],
+                          ),
+                          title: Text(state.inspectionFileInfos[index].name),
+                          subtitle:
+                              Text(state.inspectionFileInfos[index].address),
                         ),
                       );
-                    }
-                  },
-                ),
-              )
-            ],
+                    },
+                    separatorBuilder: (context, index) {
+                      return Divider(
+                        height: 0,
+                        indent: (32.0 * 2),
+                      );
+                    },
+                    itemCount: state.inspectionFileInfos.length,
+                  );
+                }
+              },
+            ),
           ),
         );
       },
